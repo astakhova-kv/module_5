e94ea5d15dd1f2f0a6474e3c619a3d250aac831f
astakhova-kv <prisakarkv@gmail.com>
2024-08-23 22:15:48 +0700

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -1,10 +1,6 @@
 class House:
     def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floors = int(number_of_floors)
-
-
-        self.name = name
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
